e65092ce5c8179419b7182b5891d92d1969c99b8
Stanley Ugwu <stanleyugwu2018@gmail.com>
2025-06-16 01:16:55 +0100

    chore: setup auth routes

--- app/auth/_layout.tsx
+++ app/auth/_layout.tsx
@@ -1,13 +1,32 @@
 import { Stack } from "expo-router";
 
+export const unstable_settings = {
+  initialRouteName: "signin",
+};
+
 export default function AuthLayout() {
   return (
-    <>
+    <Stack
+      initialRouteName="signin"
+      screenOptions={{
+        headerStyle: { backgroundColor: "transparent" },
+        headerShadowVisible: false,
+        headerTitle: "",
+        animation: "ios_from_right",
+      }}
+    >
       {/* This is the layout for the auth screens */}
-      <Stack.Screen name="signin" options={{ headerShown: false }} />
-      <Stack.Screen name="signup" options={{ headerShown: false }} />
-      <Stack.Screen name="password/forgot" options={{ headerShown: false }} />
-      <Stack.Screen name="password/reset" options={{ headerShown: false }} />
-    </>
+      <Stack.Screen name="signin" />
+
+      <Stack.Screen name="signup" />
+      <Stack.Screen name="signup/otp_verification/[email]" />
+      <Stack.Screen name="signup/otp_verified" />
+
+      <Stack.Screen name="password/forgot" />
+      <Stack.Screen name="password/forgot/otp_verification/[email]" />
+
+      <Stack.Screen name="password/reset/[email]" />
+      <Stack.Screen name="password/reset/reset_successful" />
+    </Stack>
   );
 }
