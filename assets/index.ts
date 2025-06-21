2e43ffafa59c23ffd9788156b2f00c4d339c05f7
Stanley Ugwu <stanleyugwu2018@gmail.com>
2025-06-21 22:58:46 +0100

    feat: add app icons

--- assets/index.ts
+++ assets/index.ts
@@ -1,4 +1,6 @@
 export const Images = {
   logo: require("./images/wattly_logo.png"),
+  icon: require("./images/icon.png"),
+  iconSmall: require("./images/icon_small.png"),
   success: require("./images/success_image.png"),
 };
