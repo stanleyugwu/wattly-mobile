2ae9eb91a227e4636559c03202b647ba1836be97
Stanley Ugwu <stanleyugwu2018@gmail.com>
2025-07-17 13:32:16 +0100

    feat: create util for grouping and sorting records

---
+++ src/lib/utils/groupAndSortRecords.ts
@@ -0,0 +1,33 @@
+import dayjs from "dayjs";
+
+export type GroupableRecords<R> = R[];
+
+/**
+ * Groups and sort given records by month and in a map format to be rendered in SectionList
+ */
+export function groupAndSortRecords<T>(
+  records: GroupableRecords<T>,
+  getDate: (record: T) => string
+) {
+  const grouped = records.reduce((acc, tx) => {
+    const txDate = getDate(tx);
+    const month = dayjs(txDate || new Date()).format("MMMM YYYY");
+    if (!acc[month]) acc[month] = [];
+    acc[month].push(tx);
+    return acc;
+  }, {} as Record<string, GroupableRecords<T>>);
+
+  const sortedSections = Object.entries(grouped)
+    .map(([month, data]) => ({
+      title: month,
+      data,
+    }))
+    .sort((a, b) => {
+      const dateA = dayjs(a.title, "MMMM");
+      const dateB = dayjs(b.title, "MMMM");
+
+      return dateB.valueOf() - dateA.valueOf(); // descending
+    });
+
+  return sortedSections;
+}
--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,3 +2,4 @@ export * from "./createStyleHook";
 export * from "./formatCurrency";
 export * from "./getElectricityProviderLogoFromText";
 export * from "./getFirstValidValue";
+export * from "./groupAndSortRecords";
